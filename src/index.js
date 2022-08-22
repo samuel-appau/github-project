4fa7d1c5ec1c332e7101db7a29e8c477fb74f8b4
samuel-appau <appausamuel90@gmail.com>
2022-08-22 09:14:19 -0700

    final_code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../src/views/App";
-import ApiContextProvider from "../src/context/ApiContext";
-import "assets/plugins/nucleo/css/nucleo.css";
+import App from "./App";
+import "./assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "assets/scss/argon-dashboard-react.scss";
+import "./assets/scss/argon-dashboard-react.scss";
 
-ReactDOM.render(
-  <ApiContextProvider>
-    {" "}
-    <App />
-  </ApiContextProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
